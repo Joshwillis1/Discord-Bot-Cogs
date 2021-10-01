ae87fd203c6bd381100f12c4ef7f149e4c4fad00
Josh <josh.willis1@live.com>
2021-10-01 20:03:17 +1000

    fixed it

--- deathroll/Deathroll.py
+++ deathroll/Deathroll.py
@@ -11,52 +11,45 @@ class deathroll(commands.Cog):
     @commands.command()
     async def deathroll(self, ctx, player1, player2, amount):
         """Let's play deathroll"""
-        await ctx.send(player1)
-        await ctx.send(player2)
-        await ctx.send(amount)
         playerOne = player1
         playerTwo = player2
         bet = amount
-        ready = "y"
         roll = 0
         turn = ""
+        await ctx.send("Player one = " + playerOne + " Player two = " + playerTwo + " Bet ammount = " + ('{:,}'.format(int(bet))))
 
-        while True:
-            await ctx.send("Player one = " + playerOne + " Player two = " + playerTwo + " Bet ammount = " + ('{:,}'.format(int(bet))))
+        # This randomly selects who goes first.
+        randp = randrange(1, 3, 1)
+        if randp == 1:
+            turn = playerOne
+        else:
+            turn = playerTwo
+        # Sets the initial roll
+        roll = int(bet)
+        while roll > 1:
+            # Prints who's turn it is and waits for any key.
+            await ctx.send(turn + "'s turn | " + "1 - " + ('{:,}'.format(roll))) # ('{:,}'.format(roll))
+            # Rolls the number and prints.
+            rand = randrange(1, (roll + 1), 1)
+            await ctx.send(turn + " rolls a " + str(('{:,}'.format(rand))))
+            roll = rand
+            # Checks to see if player lost
+            if roll == 1:
+                continue
+            # Changes to the next player.
+            if turn == playerOne:
+                turn = playerTwo
+            else:
+                turn = playerOne
+        # Display winner and losser 
+        winner = ""
+        losser = turn
 
-            if ready == "y":
-                # This randomly selects who goes first.
-                randp = randrange(1, 3, 1)
-                if randp == 1:
-                    turn = playerOne
-                else:
-                    turn = playerTwo
-                # Sets the initial roll
-                roll = int(bet)
-                while roll > 1:
-                    # Prints who's turn it is and waits for any key.
-                    await ctx.send(turn + "'s turn, press enter to continue | " + "1 - " + ('{:,}'.format(roll))) # ('{:,}'.format(roll))
-                    # Rolls the number and prints.
-                    rand = randrange(1, (roll + 1), 1)
-                    await ctx.send(turn + " roll's a " + str(('{:,}'.format(rand))))
-                    roll = rand
-                    # Checks to see if player lost
-                    if roll == 1:
-                        continue
-                    # Changes to the next player.
-                    if turn == playerOne:
-                        turn = playerTwo
-                    else:
-                        turn = playerOne
-                # Display winner and losser 
-                winner = ""
-                losser = turn
+        if losser == playerOne:
+            winner = playerTwo
+        else:
+            winner = playerOne
 
-                if losser == playerOne:
-                    winner = playerTwo
-                else:
-                    winner = playerOne
-
-                await ctx.send("------------------------------")
-                await ctx.send(losser + " losses and must pay " + winner + " " + ('{:,}'.format(int(bet))) + "!")
-        
+        await ctx.send("------------------------------")
+        await ctx.send(losser + " losses and must pay " + winner + " " + ('{:,}'.format(int(bet))) + "!")           
+    
