84a9cc9ec67f841095e4f8bccb129a48dc49bf5e
Josh <josh.willis1@live.com>
2021-10-01 19:47:57 +1000

    removed inputs and changed print to await ctx.send

--- deathroll/Deathroll.py
+++ deathroll/Deathroll.py
@@ -9,39 +9,18 @@ class deathroll(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def deathroll(self, ctx, *, entered_title):
+    async def deathroll(self, ctx, *, player1, player2, amount):
         """Let's play deathroll"""
-        await ctx.send("I can do stuff!"+entered_title)
-        return
-        playerOne = ""
-        playerTwo = ""
-        bet = ""
-        ready = ""
+
+        playerOne = player1
+        playerTwo = player2
+        bet = amount
+        ready = "y"
         roll = 0
         turn = ""
 
-        def getPlayers():
-            global playerOne, playerTwo
-            print("Player one enter your name here:")
-            playerOne = input()
-            print("Player one = " + playerOne)
-            print("Player two enter your name here:")
-            playerTwo = input()
-            print("Player two = " + playerTwo)
-
-        def getBet():
-            global bet
-            print("Bet ammount:")
-            bet = input()
-            print("bet ammount = " + ('{:,}'.format(int(bet))))
-
-        getPlayers()
-        getBet()
-
         while True:
-            print("Player one = " + playerOne + " Player two = " + playerTwo + " Bet ammount = " + ('{:,}'.format(int(bet))))
-            print("Are you ready? y/n")
-            ready = input()
+            await ctx.send("Player one = " + playerOne + " Player two = " + playerTwo + " Bet ammount = " + ('{:,}'.format(int(bet))))
 
             if ready == "y":
                 # This randomly selects who goes first.
@@ -54,11 +33,10 @@ class deathroll(commands.Cog):
                 roll = int(bet)
                 while roll > 1:
                     # Prints who's turn it is and waits for any key.
-                    print(turn + "'s turn, press enter to continue | " + "1 - " + ('{:,}'.format(roll))) # ('{:,}'.format(roll))
-                    input()
+                    await ctx.send(turn + "'s turn, press enter to continue | " + "1 - " + ('{:,}'.format(roll))) # ('{:,}'.format(roll))
                     # Rolls the number and prints.
                     rand = randrange(1, (roll + 1), 1)
-                    print(turn + " roll's a " + str(('{:,}'.format(rand))))
+                    await ctx.send(turn + " roll's a " + str(('{:,}'.format(rand))))
                     roll = rand
                     # Checks to see if player lost
                     if roll == 1:
@@ -77,19 +55,6 @@ class deathroll(commands.Cog):
                 else:
                     winner = playerOne
 
-                print("------------------------------")
-                print(losser + " losses and must pay " + winner + " " + ('{:,}'.format(int(bet))) + "!")
-
-            print("Would you like to play again?")
-            again = input()
-            if again == "y":
-                print("Would you like to use the same names?")
-                again = input()
-                if again == "y":
-                    getBet()
-                else:
-                    getPlayers()
-                    getBet()
-            else:
-                break
+                await ctx.send("------------------------------")
+                await ctx.send(losser + " losses and must pay " + winner + " " + ('{:,}'.format(int(bet))) + "!")
         
