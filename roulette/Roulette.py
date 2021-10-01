28758d4cee02970fcc31e14e6ee86d1db7979bae
Josh <josh.willis1@live.com>
2021-10-01 23:18:19 +1000

    debugging user response to bullets

--- roulette/Roulette.py
+++ roulette/Roulette.py
@@ -13,6 +13,8 @@ class roulette(commands.Cog):
     async def roulette(self, ctx):
         """Feeling lucky?"""
 
+        chambers = ""
+
         check = lambda m: (
             not m.author.bot
             and m.channel == ctx.message.channel
@@ -22,21 +24,13 @@ class roulette(commands.Cog):
             )
         )
 
-        chambers = await ctx.send('Enter number of bullets (2-6)')
+        await ctx.send('Enter number of bullets (2-6)')
 
         try:
             r = await self.bot.wait_for('message', timeout=60, check=check)
-            if int(r) > 6:
-                await ctx.sent(r + 'greater')
-                return await ctx.send('Bullets must be less than or equal to 6')
-            elif int(r) < 2:
-                await ctx.sent(r + 'lower')
-                return await ctx.send('Bullets must be greater than or equal to 2')
-            elif not r.isdigit():
-                await ctx.sent(r + 'not digit')
-                return await ctx.send('Bullets must be a number betweeen 2 and 6')
-            else:
-                chambers = int(r)
+            chambers = int(r)
+            await ctx.send (chambers)
+            
         except asyncio.TimeoutError:
             return await ctx.send('Timed out, try again')
 
