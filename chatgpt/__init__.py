e667046d901e27c3ddbe7eccb8963066bb2a2d18
Josh <josh.willis1@live.com>
2023-04-30 16:14:07 +1000

    yeet

--- chatgpt/__init__.py
+++ chatgpt/__init__.py
@@ -2,5 +2,8 @@ from .Chatgpt import TalkToChatGPTCog
 from redbot.core.bot import Red
 
 def setup(bot: Red):
+    cog = TalkToChatGPTCog(bot)
+    bot.add_cog(cog)   
+    
     #cog = TalkToChatGPTCog(bot)
-    bot.add_cog(TalkToChatGPTCog(bot))
+    #bot.add_cog(TalkToChatGPTCog(bot))
