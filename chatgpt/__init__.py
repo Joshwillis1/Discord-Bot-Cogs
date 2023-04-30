4c56b280fa0d4df74e31366edbe72125ae4b7417
Josh <josh.willis1@live.com>
2023-04-30 15:12:52 +1000

    test

--- chatgpt/__init__.py
+++ chatgpt/__init__.py
@@ -2,5 +2,5 @@ from .Chatgpt import TalkToChatGPTCog
 from redbot.core.bot import Red
 
 async def setup(bot: Red):
-    cog = chatgpt(bot)
+    cog = TalkToChatGPTCog(bot)
     await bot.add_cog(cog)
