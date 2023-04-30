30491857251f97201c5b9b5a6bfd63c0388e7eaa
Josh <josh.willis1@live.com>
2023-04-30 20:09:00 +1000

    yeet

--- chatgpt/Chatgpt.py
+++ chatgpt/Chatgpt.py
@@ -5,6 +5,7 @@ from redbot.core.bot import Red
 import openai
 import os
 import asyncio
+import logging
 
 
 class TalkToChatGPTCog(commands.Cog):
@@ -13,6 +14,20 @@ class TalkToChatGPTCog(commands.Cog):
         self.api_key = os.environ.get("OPENAI_API_KEY")
         openai.api_key = self.api_key
         self.prev_ai_message = None
+        self.logger = logging.getLogger("chat_gpt_logger")
+
+        # set the logger level
+        self.logger.setLevel(logging.DEBUG)
+
+        # create a file handler
+        handler = logging.FileHandler("chat_gpt.log")
+
+        # set the formatter for the handler
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        handler.setFormatter(formatter)
+
+        # add the handler to the logger
+        self.logger.addHandler(handler)
 
     @commands.command()
     async def talk(self, ctx, *, message):
@@ -20,8 +35,10 @@ class TalkToChatGPTCog(commands.Cog):
         if isinstance(response, tuple):
             error_message = response[1]
             await ctx.send(error_message)
+            self.logger.error(error_message)
         else:
             await ctx.send(response)
+            self.logger.info(f"User: {message}\nAI: {response}")
             self.prev_ai_message = response
 
     def get_response(self, message, prev_ai_message=None):
@@ -41,7 +58,7 @@ class TalkToChatGPTCog(commands.Cog):
 
             return ai_message
         except Exception as e:
-            print(e)
+            self.logger.error(str(e))
             return e
 
     @commands.command()
@@ -57,14 +74,16 @@ class TalkToChatGPTCog(commands.Cog):
                 )
                 if user_input.content.lower() == "exit":
                     await ctx.send("Goodbye!")
+                    self.logger.info("User: exit\nAI: Goodbye!")
                     break
                 response = self.get_response(user_input.content, prev_ai_message)
                 prev_ai_message = response.split("\n")[-1].strip()  # get last AI message
                 await ctx.send(f"AI: {response}")
+                self.logger.info(f"User: {user_input.content}\nAI: {response}")
             except asyncio.TimeoutError:
                 await ctx.send("Sorry, I didn't receive any message in the last 60 seconds. The chat has ended.")
+                self.logger.info("TimeoutError: The chat has ended.")
                 break
 
 def setup(bot):
     cog = TalkToChatGPTCog(bot)
-    
